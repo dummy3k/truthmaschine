24b03ac2d1e5ddec69660d1c895f7a99df9a288e
Artur Hallmann <arturh@arturh.de>
2009-09-04 21:52:28 +0200

    changed project settings for SQL-Alchemy

--- thetruth/thetruth/websetup.py
+++ thetruth/thetruth/websetup.py
@@ -2,9 +2,13 @@
 import logging
 
 from thetruth.config.environment import load_environment
+from thetruth.model import meta
 
 log = logging.getLogger(__name__)
 
 def setup_app(command, conf, vars):
     """Place any commands to setup thetruth here"""
     load_environment(conf.global_conf, conf.local_conf)
+
+    # Create the tables if they don't already exist
+    meta.metadata.create_all(bind=meta.engine)

d4b37748723f97ff41740f4216574854d2208b33
Artur Hallmann <arturh@arturh.de>
2009-09-17 20:03:49 +0200

    removed the need for an env var

--- delete-and-initialize-sqlite-db.bat
+++ delete-and-initialize-sqlite-db.bat
@@ -5,26 +5,11 @@ SET DATABASE_FILE=database-dev.db
 SET CONFIG=development.ini
 SET DATA_FOLDER=data
 
-IF NOT DEFINED PYLONS_VIRTUALDEV (
-	ECHO Environment Variable PYLONS_VIRTUALDEV not definied
-	GOTO exit
-)
-
-ECHO ============================================
-ECHO Using PYLONS_VIRTUALDEV=%PYLONS_VIRTUALDEV%
-
-
-IF NOT EXIST "%PYLONS_VIRTUALDEV%" (
-	ECHO Directory %PYLONS_VIRTUALDEV% does not exist
-	GOTO exit
-)
-
 IF EXIST "%DATABASE_FILE%" (
     ECHO ============================================
     ECHO Deleting old Database "%DATABASE_FILE%"
     DEL %DATABASE_FILE%
 )
-
 
 IF EXIST "%DATA_FOLDER%" (
     ECHO ============================================
@@ -34,8 +19,9 @@ IF EXIST "%DATA_FOLDER%" (
 
 ECHO ============================================
 ECHO Running "setup-app" command to create the new database
+ECHO ============================================
 
-"%PYLONS_VIRTUALDEV%\Scripts\paster" setup-app %CONFIG%
+paster setup-app %CONFIG%
 
 GOTO EXIT
 
--- start-test-webserver.bat
+++ start-test-webserver.bat
@@ -5,25 +5,11 @@ SET DATABASE_FILE=database-dev.db
 SET CONFIG=development.ini
 SET DATA_FOLDER=data
 
-IF NOT DEFINED PYLONS_VIRTUALDEV (
-	ECHO Environment Variable PYLONS_VIRTUALDEV not definied
-	GOTO exit
-)
-
-ECHO ============================================
-ECHO Using PYLONS_VIRTUALDEV=%PYLONS_VIRTUALDEV%
-
-
-IF NOT EXIST "%PYLONS_VIRTUALDEV%" (
-	ECHO Directory %PYLONS_VIRTUALDEV% does not exist
-	GOTO exit
-)
-
 ECHO ============================================
 ECHO Running "paster serve --reload %CONFIG%"
+ECHO ============================================
 
-"%PYLONS_VIRTUALDEV%\Scripts\paster" serve --reload %CONFIG%"
-
+paster serve --reload %CONFIG%"
 GOTO EXIT
 
 :exit

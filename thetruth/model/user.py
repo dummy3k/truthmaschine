e719d2707cfb213ec66b8ebdbaff74fc4aa7280f
dummy3k <l4711@gmx.net>
2009-09-24 20:26:18 +0200

    put model classes in their own files

---
+++ thetruth/model/user.py
@@ -0,0 +1,39 @@
+from sqlalchemy import *
+from thetruth.model import meta
+import hashlib
+
+users_table = Table('users', meta.metadata,
+    Column('id', Integer, primary_key=True),
+    Column('name', String(100)),
+    Column('email', String(100)),
+    Column('openid', String(255)),
+    Column('banned', Boolean),
+    Column('reputation', Integer),
+    Column('signup', DateTime),
+    Column('last_login', DateTime),
+)
+
+class User(object):
+    def __unicode__(self):
+        return self.name
+
+    def getHashedEmailAddress(self):
+        if self.email:
+            return hashlib.md5(self.email.strip().lower()).hexdigest()
+        else:
+            return hashlib.md5(self.openid.strip().lower()).hexdigest()
+        
+    def getDisplayName(self):
+        if self.name:
+            return self.name
+        else:
+            return "Unnamed User"
+
+    __str__ = __unicode__
+
+    def __repr__(self):
+        return "<User('%s', '%s')>" % (self.name, self.openid)
+
+    def updatelastlogin(self):
+        pass
+    

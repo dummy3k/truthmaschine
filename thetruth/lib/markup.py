e141c5d54c06aecaa9bd481f150e1e0c7306bf77
dummy3k <l4711@gmx.net>
2009-10-04 21:07:06 +0200

    simplified renderMarkup()

--- thetruth/lib/markup.py
+++ thetruth/lib/markup.py
@@ -4,18 +4,11 @@ import logging
 log = logging.getLogger(__name__)
 
 def renderMarkup(s):
-    # order matters
-    log.debug("0: " + s)
-    s = re.sub('^(http://[^\\s]+?)$', '<a href="\\1">\\1</a>', s)
-    log.debug("1: " + s)
-    s = re.sub('(\\s)(http://[^\\s]+?)$', '\\1<a href="\\2">\\2</a>', s)
-    log.debug("2: " + s)
-    s = re.sub('^(http://[^\\s]+?)(\\s)', '<a href="\\1">\\1</a>\\2', s)
-    log.debug("3: " + s)
-    s = re.sub('(\\s)(http://.+?)(\\s)', '\\1<a href="\\2">\\2</a>\\3', s)
-    log.debug("4: " + s)
     s = re.sub('\[(.+?)\|(.+?)\]', '<a href="\\1">\\2</a>', s)
-    log.debug("5: " + s)
+    log.debug("1: " + s)
+    
+    s = re.sub('(^|\\s)(http://[^\\s "<>]+)($|\\s)', '\\1<a href="\\2">\\2</a>\\3', s)
+    log.debug("2: " + s)
     return s
     
 def stripMarkup(s):
@@ -25,4 +18,7 @@ def stripMarkup(s):
 def stripMarkupAndTruncate(s):
     s = re.sub('\[(.+?)\|(.+?)\]', '\\2', s)
     return s[:100]
+    
+if __name__ == '__main__':
+    unittest.main()
     

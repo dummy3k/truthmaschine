9bb197d28478d89b2f32da1a22c695b3cfed7efe
Artur Hallmann <arturh@arturh.de>
2009-10-15 21:11:02 +0200

    fixed the fix (or did I?)

--- thetruth/lib/search.py
+++ thetruth/lib/search.py
@@ -9,7 +9,7 @@ from whoosh.qparser import QueryParser
 
 class Search():
     def __init__(self):
-        self.schema = Schema(message=TEXT(stored=True), id=ID(stored=True, id=True))
+        self.schema = Schema(message=TEXT(stored=False), id=ID(stored=True))
 
         index_path = "data/index"
         self.new_index = False
